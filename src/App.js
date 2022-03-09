283c0dd73c7b9e2fcbec80320c8e9bd5d5266724
Gabriel <rochafrgabriel@gmail.com>
2022-03-09 00:05:49 -0300

    src files delete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Header} from './components/Header.js'
+import {WatchList} from './components/WatchList.js'
+import {Watchedmovies} from './components/Watchedmovies.js'
+import {Add} from './components/Add.js'
 import './App.css';
+import './lib/font-awesome/css/all.min.css'
+import {GlobalProvider} from './Context/GlobalState'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Router>
+        <Header/>
+
+        <Switch>
+          <Route exact path="/">
+            <WatchList/>  
+          </Route>
+          
+          <Route path="/watchedmovies">
+            <Watchedmovies/>  
+          </Route>
+
+          <Route path="/add">
+            <Add/>  
+          </Route>
+        </Switch>
+
+      </Router>
+    </GlobalProvider>
   );
 }
 
