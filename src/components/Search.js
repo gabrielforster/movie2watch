639a2571c9933f15a4bbf5bb3b2343c87c5dda9a
Gabriel <rochafrgabriel@gmail.com>
2022-03-10 00:21:14 -0300

    fixing API key

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,10 +6,12 @@ export const Search = () => {
   const[search, setSearch] = useState("");
   const[movies, setMovies] = useState([])
 
+  let apiKey = '80b5bde829cf84ab43064490f20c934e'
+
   const searchMovies=(evt) => {
     if(evt.key === "Enter")
     {
-      fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=pt-br&page=1&include_adult=false&query=${search}&max-results=40`)
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=pt-br&page=1&include_adult=false&query=${search}`)
       .then(res=> res.json())
       .then(data=>{
     if(!data.errors){
